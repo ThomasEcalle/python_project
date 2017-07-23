870fd52d44d696c0be5719c2432b305ac0dc052d
ThomasEcalle <thomasecalle@gmail.com>
2017-07-23 04:32:20 +0200

    added last comments

--- website/views/deconnect.py
+++ website/views/deconnect.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from django.shortcuts import redirect
+
+
 # This file contains the deconnection handling page route.
 
 def deconnect(request):
-	print("yahoooooooooooo ")
-	logout(request)
-	return redirect('logon')
-	
+    """
+        a deconnection link.
+        Notice the CACHE element on settings.py.
+        We added a dependancy in order to remove the dummy cache
+        that can be an issue in order to process logout
+    """
+    print("yahoooooooooooo ")
+    logout(request)
+    return redirect('logon')
--- website/views/home.py
+++ website/views/home.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
 
 @login_required
 def home(request):
+    """ show a basic home page"""
     messages.add_message(request, messages.DEBUG, 'Test message !')
     return render(request, 'home.html', {})
